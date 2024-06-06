e535c237db12294d32fb85c92beda75dcee33132
RICIT2024 <ricit@sectur.gob.mx>
2024-06-06 17:03:17 -0600

    Add files via upload

---
+++ Estancias/views/user/index.php
@@ -0,0 +1,52 @@
+<?php
+    use ricit\humhub\modules\Estancias\models\EstanciasInvestigacion;
+    use yii\helpers\Html;
+    use yii\helpers\Url;
+    use yii\grid\ActionColumn;
+    use yii\grid\GridView;
+    use yii\db\Query;
+    
+?>
+<div class="panel panel-default">
+    <div class="panel-heading">
+        <?= Yii::t('UserModule.profile', '<strong>Estancias de Investigación</strong>') ?>
+    </div>
+
+<?php   
+   echo GridView::widget([
+    'dataProvider' => $dataProvider,
+    'columns' =>[
+        [
+            'attribute' => 'User_id',
+            'headerOptions'=>[ 'style'=>'background-color:#BC955C; text-align: center;' ],
+            'value' => function ($model) {
+                $profile = (new Query())
+                    ->select(['firstname', 'lastname'])
+                    ->from('profile')
+                    ->where(['user_id' => $model->User_id])
+                    ->one();
+
+                return $profile ? $profile['firstname'] . ' ' . $profile['lastname'] : '';
+            },
+        ],
+         [            
+            // you may configure additional properties here
+            'attribute' => 'Institucion',
+            'headerOptions'=>[ 'style'=>'background-color:#BC955C; text-align: center;' ]
+        ],
+        [            
+         // you may configure additional properties here
+         'attribute' => 'Pais',
+         'headerOptions'=>[ 'style'=>'background-color:#BC955C; text-align: center;' ]
+         ],
+         [            
+            // you may configure additional properties here
+            'attribute' => 'Periodo',
+            'headerOptions'=>[ 'style'=>'background-color:#BC955C; text-align: center;' ]
+        ],
+    
+      ],
+     ]);
+
+?>
+</div>
