be409b55316c9a153342f7bf818ef39826f3daa5
RICIT2024 <ricit@sectur.gob.mx>
2024-06-11 17:49:06 -0600

    Add files via upload

--- test/views/user/index.php
+++ test/views/user/index.php
@@ -10,43 +10,75 @@
 ?>
 <div class="panel panel-default">
     <div class="panel-heading">
-        <?= Yii::t('UserModule.profile', '<strong>Producción Cientifica</strong>') ?>
+        <?= Yii::t('UserModule.profile', '<strong>Producción Científica</strong>') ?>
     </div>
 
-<?php   
-
-   echo GridView::widget([
-    'dataProvider' => $dataProvider,
-    'columns' =>[
-        
-         [            
-            // you may configure additional properties here
-            'attribute' => 'Autor',
-            'headerOptions'=>[ 'style'=>'background-color:#BC955C; text-align: center;' ]
+    <h2>Artículos</h2>
+    <?= GridView::widget([
+        'dataProvider' => $dataProviderArticulos,
+        'columns' => [
+            'Articulos_id',
+            'Autor',
+            'Autores',
+            'Anio',
+            'Titulo',
+            'Resumen',
+            // Agrega más columnas si es necesario
         ],
-        [            
-         // you may configure additional properties here
-         'attribute' => 'Anio',
-         'label' =>'Año',
-         'headerOptions'=>[ 'style'=>'background-color:#BC955C; width:6%; text-align: center;' ]
-         ],
-         [            
-            // you may configure additional properties here
-            'attribute' => 'Titulo',
-            'headerOptions'=>[ 'style'=>'background-color:#BC955C; text-align: center;' ]
-        ],
-        [            
-         // you may configure additional properties here
-         'attribute' => 'Resumen',
-         'headerOptions'=>[ 'style'=>'background-color:#BC955C; text-align: center;' ]
-         ],
-         [            
-            // you may configure additional properties here
-            'attribute' => 'Tipo',
-            'headerOptions'=>[ 'style'=>'background-color:#BC955C; width:8%; text-align: center;' ]
-         ],
-      ],
-     ]);
+    ]) ?>
 
-?>
+    <h2>Libros</h2>
+    <?= \yii\grid\GridView::widget([
+        'dataProvider' => $dataProviderLibros,
+        'columns' => [
+            'Libro_id',
+            'Autor',
+            'Autores_sec',
+            'Anio',
+            'Titulo',
+            'Resumen',
+            // Agrega más columnas si es necesario
+        ],
+    ]) ?>
+
+    <h2>Capítulos de Libros</h2>
+    <?= \yii\grid\GridView::widget([
+        'dataProvider' => $dataProviderCapLibros,
+        'columns' => [
+            'Cap_id',
+            'Autor_libro',
+            'Autores_capitulo',
+            'Anio',
+            'Titulo_capitulo',
+            'Resumen',
+            // Agrega más columnas si es necesario
+        ],
+    ]) ?>
+
+    <h2>Ponencias</h2>
+    <?= \yii\grid\GridView::widget([
+        'dataProvider' => $dataProviderPonencias,
+        'columns' => [
+            'Ponencia_id',
+            'Autor',
+            'Anio',
+            'Titulo_ponencia',
+            'Resumen',
+            // Agrega más columnas si es necesario
+        ],
+    ]) ?>
+
+    <h2>Tesis</h2>
+    <?= \yii\grid\GridView::widget([
+        'dataProvider' => $dataProviderTesis,
+        'columns' => [
+            'Tesis_id',
+            'Autor',
+            'Grado_academico',
+            'Institucion_procedencia',
+            'Anio',
+            'Titulo',
+            // Agrega más columnas si es necesario
+        ],
+    ]) ?>
 </div>
