a2bf2c406daa45328086ba599dac14a8d1a13e32
RICIT2024 <ricit@sectur.gob.mx>
2024-06-28 13:11:06 -0600

    Add files via upload

--- test/controllers/IndexController.php
+++ test/controllers/IndexController.php
@@ -2,57 +2,24 @@
 
 namespace ricit\humhub\modules\test\controllers;
 
-use humhub\components\Controller;
-
-use ricit\humhub\modules\test\models\SearchA;
-use ricit\humhub\modules\test\models\SearchC;
-use ricit\humhub\modules\test\models\SearchP;
-use ricit\humhub\modules\test\models\SearchSP;
+use Yii;
+use yii\web\Controller;
+use ricit\humhub\modules\test\models\SearchM;
 
 class IndexController extends Controller
 {
-
-    public $subLayout = "@test/views/layouts/default";
-
-    /**
-     * Renders the index view for the module
-     *
-     * @return string
-     */
     public function actionIndex()
     {
-        //Capitulo de libro
-        $searchModelC = new SearchC();
-        $dataProviderC = $searchModelC->searchAll($this->request->queryParams);
-        //Articulo
-        $searchModelA = new SearchA();
-        $dataProviderA = $searchModelA->searchAll($this->request->queryParams);
-        //Libro
-        $searchModelL = new SearchSP();
-        $dataProviderL = $searchModelL->searchAll($this->request->queryParams);
-        //Ponencia
-        $searchModelP = new SearchP();
-        $dataProviderP = $searchModelP->searchAll($this->request->queryParams);
+        $searchModel = new SearchM();
+        $dataProviders = null;
 
-        $combinedDataProvider = [
-            'dataProvider1' => $dataProviderC,
-            'dataProvider2' => $dataProviderA,
-            'dataProvider3' => $dataProviderL,
-            'dataProvider4' => $dataProviderP,
-        ];
+        if (Yii::$app->request->queryParams) {
+            $dataProviders = $searchModel->search(Yii::$app->request->queryParams);
+        }
 
-        $combinedSearch = [
-            'searchModel1' => $dataProviderC,
-            'searchModel2' => $searchModelA,
-            'searchModel3' => $searchModelL,
-            'searchModel4' => $searchModelP,
-        ];
-
-        return $this->render('index',[
-        'combinedDataProvider' => $combinedDataProvider,
-        'combinedSearch' => $combinedSearch,
-        ]);        
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProviders' => $dataProviders,
+        ]);
     }
-
 }
-
