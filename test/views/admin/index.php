7fc5e09992b1e159937ac1f5ef350f2dce6d6a43
RICIT2024 <ricit@sectur.gob.mx>
2025-03-28 16:34:42 -0600

    Add files via upload

--- test/views/admin/index.php
+++ test/views/admin/index.php
@@ -3,6 +3,8 @@
 use yii\helpers\Html;
 use yii\grid\ActionColumn;
 use yii\grid\GridView;
+use yii\helpers\Url;
+
 use ricit\humhub\modules\test\models\Ponencias;
 
 
@@ -22,7 +24,8 @@ $ponenciaModel = new Ponencias();
 <div class="container-fluid">
     <div class="panel panel-default">
         
-        <?= $this->render('@test/views/layouts/nav-header', []) ?>
+    <?php // echo $this->render('@test/views/layouts/nav-header', []); ?>
+
 
         <div class="panel-body">
             <div id="row one">
@@ -32,69 +35,33 @@ $ponenciaModel = new Ponencias();
                     <div style="width:100px; display: flex; flex-direction: column; justify-content:space-around; align-items:center; background-color: #fff; border: 0.2rem solid; padding:5px; border-radius: 10px;">
                         <i class="fa fa-book"></i>    
                         <p> <?= $model->getPonenciasTotal(); ?> </p>
-                        <h6>Ponencia</h6>
-                    </div>
+                        <?= Html::a('Ponencias', Url::to(['ponencias/index']), ['style' => 'font-weight: bold; ']) ?>
+                        </div>
                     <div style="width:100px; display: flex; flex-direction: column; justify-content:space-around; align-items:center; background-color: #fff; border: 0.2rem solid; padding:5px; border-radius: 10px;">
                         <i class="fa fa-book"></i>    
                         <p> <?= $model->getArticulosTotal(); ?></p>
-                        <h6>Articulos</h6>
+                        <?= Html::a('Artículos', Url::to(['articulos/index']), ['style' => 'font-weight: bold; ']) ?>
                     </div>
                     <div style="width:100px; display: flex; flex-direction: column; justify-content:space-around; align-items:center; background-color: #fff; border: 0.2rem solid; padding:5px; border-radius: 10px;">
                         <i class="fa fa-book"></i>    
                         <p> <?= $model->getLibrosTotal(); ?></p>
-                        <h6>Libros</h6>
-                    </div>
+                        <?= Html::a('Libros', Url::to(['investigations/index']), ['style' => 'font-weight: bold; ']) ?>
+                        </div>
                     <div style="width:100px; display: flex; flex-direction: column; justify-content:space-around; align-items:center; background-color: #fff; border: 0.2rem solid; padding:5px; border-radius: 10px;">
                         <i class="fa fa-book"></i>    
                         <p> <?= $model->getCapitulosTotal(); ?></p>
-                        <h6>Capitulos</h6>
+                        <?= Html::a('Capítulos', Url::to(['capitulos/index']), ['style' => 'font-weight: bold; ']) ?>
                     </div>
                     <div style="width:100px; display: flex; flex-direction: column; justify-content:space-around; align-items:center; background-color: #fff; border: 0.2rem solid; padding:5px; border-radius: 10px;">
                         <i class="fa fa-book"></i>    
                         <p> <?= $model->getTesisTotal(); ?></p>
-                        <h6>Tesis</h6>
-                    </div>
+                        <?= Html::a('Tesis', Url::to(['tesis/index']), ['style' => 'font-weight: bold; ']) ?>
+                        </div>
                 
                 </div>
             </div>
             <br>
-            <div id="row two">
-                <p style="text-align: center; font-weight:bold;"><?= Yii::t('TestModule.base', 'Ponencias Por Tipo.') ?></p>
-                <div style="display: flex; gap:25px; justify-content:center; align-items:center; flex-wrap: wrap;" id="Values">
-        
-                    <div style="width:100px; display: flex; flex-direction: column; justify-content:space-around; align-items:center; background-color: #fff; border: 0.2rem solid; padding:5px; border-radius: 10px;">
-                        <i class="fa fa-book"></i>    
-                        <p> <?= $ponenciaModel->getTotalCongreso(); ?> </p>
-                        <h6>Congreso</h6>
-                    </div>
-                    <div style="width:100px; display: flex; flex-direction: column; justify-content:space-around; align-items:center; background-color: #fff; border: 0.2rem solid; padding:5px; border-radius: 10px;">
-                        <i class="fa fa-book"></i>    
-                        <p> <?= $ponenciaModel->getTotalSimposio(); ?></p>
-                        <h6>Simposio</h6>
-                    </div>
-                    <div style="width:100px; display: flex; flex-direction: column; justify-content:space-around; align-items:center; background-color: #fff; border: 0.2rem solid; padding:5px; border-radius: 10px;">
-                        <i class="fa fa-book"></i>    
-                        <p> <?= $ponenciaModel->getTotalForo(); ?></p>
-                        <h6>Foro</h6>
-                    </div>
-                    <div style="width:100px; display: flex; flex-direction: column; justify-content:space-around; align-items:center; background-color: #fff; border: 0.2rem solid; padding:5px; border-radius: 10px;">
-                        <i class="fa fa-book"></i>    
-                        <p> <?= $ponenciaModel->getTotalSeminario(); ?></p>
-                        <h6>Seminario</h6>
-                    </div>
-                    <div style="width:100px; display: flex; flex-direction: column; justify-content:space-around; align-items:center; background-color: #fff; border: 0.2rem solid; padding:5px; border-radius: 10px;">
-                        <i class="fa fa-book"></i>    
-                        <p> <?= $ponenciaModel->getTotalPanel(); ?></p>
-                        <h6>Panel</h6>
-                    </div>
-                    <div style="width:100px; display: flex; flex-direction: column; justify-content:space-around; align-items:center; background-color: #fff; border: 0.2rem solid; padding:5px; border-radius: 10px;">
-                        <i class="fa fa-book"></i>    
-                        <p> <?= $ponenciaModel->getTotalOtro(); ?></p>
-                        <h6>Otro</h6>
-                    </div>
-                
-                </div>
-            </div>    
+            
                 
             <div>
                     <h1 style="margin-top:10px; font-weight:bold; font-size:20px; width:auto; text-align:center;">
